dcce27fde7e848f1fcbc6e5ddc9f5fba3b41a281
Andrew Wason <rectalogic@rectalogic.com>
2026-09-15 10:58:12 -0400

    make perfcounter optional

--- src/demos/cube.rs
+++ src/demos/cube.rs
@@ -3,6 +3,16 @@ extern crate alloc;
 use alloc::vec;
 use alloc::vec::Vec;
 use embassy_time::{Duration, Instant, Timer};
+#[cfg(feature = "cube-perf")]
+use embedded_3dgfx::perfcounter::PerformanceCounter;
+#[cfg(feature = "cube-perf")]
+use embedded_graphics::{
+    Drawable,
+    geometry::Point,
+    mono_font::{MonoTextStyle, ascii::FONT_6X10},
+    text::Text,
+};
+
 use embedded_3dgfx::{
     Z_MAX_VALUE,
     command_buffer::CommandBuffer,
@@ -78,9 +88,19 @@ pub async fn _render(mut framebuffer: FrameBuffer) -> ! {
     cube.set_render_mode(RenderMode::Lines);
     cube.set_color(Rgb565::CSS_CYAN);
 
+    #[cfg(feature = "cube-perf")]
+    let (mut perf, text_style) = {
+        let mut perf = PerformanceCounter::new();
+        perf.only_fps(true);
+        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::CSS_WHITE);
+        (perf, text_style)
+    };
+
     let initial_time = Instant::now();
 
     loop {
+        #[cfg(feature = "cube-perf")]
+        perf.start_of_frame();
         let render_time = Instant::now();
         // Calculate rotation based on time
         let elapsed = initial_time.elapsed().as_secs() as f32;
@@ -103,6 +123,14 @@ pub async fn _render(mut framebuffer: FrameBuffer) -> ! {
         engine
             .execute(&mut framebuffer, &mut frame, &commands, None)
             .unwrap();
+
+        #[cfg(feature = "cube-perf")]
+        {
+            perf.print();
+            Text::new(perf.get_text(), Point::new(10, 20), text_style)
+                .draw(&mut framebuffer)
+                .unwrap();
+        }
 
         framebuffer = render_buffer(framebuffer).await;
         let render_elapsed = render_time.elapsed().as_millis();
