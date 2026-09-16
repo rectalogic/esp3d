ed04d40fbda1846c41b031585fda9395c3f58510
Andrew Wason <rectalogic@rectalogic.com>
2026-09-16 15:04:33 +0000

    make main a library

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,96 +1,9 @@
 #![no_std]
 #![no_main]
-#![deny(
-    clippy::mem_forget,
-    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
-    holding buffers for the duration of a data transfer."
-)]
-#![deny(clippy::large_stack_frames)]
 
 use embassy_executor::Spawner;
-use esp_hal::{clock::CpuClock, system::Stack, timer::timg::TimerGroup};
-use esp_rtos::embassy::Executor;
-use static_cell::StaticCell;
 
-use log::info;
-
-use esp_backtrace as _;
-
-extern crate alloc;
-
-// This creates a default app-descriptor required by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
-esp_bootloader_esp_idf::esp_app_desc!();
-
-#[allow(
-    clippy::large_stack_frames,
-    reason = "it's not unusual to allocate larger buffers etc. in main"
-)]
 #[esp_rtos::main]
-async fn main(_spawner: Spawner) -> ! {
-    // generator version: 1.3.0
-    // generator parameters: --chip esp32s3 -o esp32s3-wroom-1-octal-psram -o unstable-hal -o alloc -o embassy -o log -o esp-backtrace -o zed
-
-    esp_println::logger::init_logger_from_env();
-
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let peripherals = esp_hal::init(config);
-
-    // The following pins are used to bootstrap the chip. They are available
-    // for use, but check the datasheet of the module for more information on them.
-    // - GPIO0
-    // - GPIO3
-    // - GPIO45
-    // - GPIO46
-    // These GPIO pins are in use by some feature of the module and should not be used.
-    let _ = peripherals.GPIO27;
-    let _ = peripherals.GPIO28;
-    let _ = peripherals.GPIO29;
-    let _ = peripherals.GPIO30;
-    let _ = peripherals.GPIO31;
-    let _ = peripherals.GPIO32;
-    let _ = peripherals.GPIO33;
-    let _ = peripherals.GPIO34;
-    let _ = peripherals.GPIO35;
-    let _ = peripherals.GPIO36;
-    let _ = peripherals.GPIO37;
-
-    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 73744);
-    esp_alloc::psram_allocator!(peripherals.PSRAM, esp_hal::psram);
-
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    esp_rtos::start(timg0.timer0, peripherals.FROM_CPU_INTR0);
-
-    info!("Embassy initialized!");
-
-    let display_peripherals = esp3d::display::Peripherals {
-        spi2: peripherals.SPI2,
-        dma: peripherals.DMA_CH0.into(),
-        dc: peripherals.GPIO46.into(),
-        mosi: peripherals.GPIO11.into(),
-        sclk: peripherals.GPIO12.into(),
-        cs: peripherals.GPIO10.into(),
-        bl: peripherals.GPIO45.into(),
-    };
-    static APP_CORE_STACK: StaticCell<Stack<8192>> = StaticCell::new();
-    let app_core_stack = APP_CORE_STACK.init(Stack::new());
-    esp_rtos::start_second_core(
-        peripherals.CPU_CTRL,
-        peripherals.FROM_CPU_INTR1,
-        app_core_stack,
-        move || {
-            static EXECUTOR: StaticCell<Executor> = StaticCell::new();
-            let executor = EXECUTOR.init(Executor::new());
-            executor.run(|spawner| {
-                spawner.spawn(
-                    esp3d::swapchain::swapchain_task(display_peripherals)
-                        .expect("spawn swapchain_task"),
-                );
-            });
-        },
-    );
-    let swapchain = esp3d::swapchain::SwapChain::new();
-    esp3d::demos::render(swapchain).await
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
+async fn main(spawner: Spawner) -> ! {
+    esp3d::app::app(spawner).await
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+pub mod app;
 pub mod demos;
 pub mod display;
 pub mod swapchain;
